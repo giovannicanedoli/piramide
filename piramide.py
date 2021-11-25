43645800eef2d5cac44b1f2267676c6e8b8acff1
giovannicanedoli <90329991+giovannicanedoli@users.noreply.github.com>
2021-11-25 15:56:56 +0100

    PRIMO UPDATE PIRAMIDE

---
+++ piramide.py
@@ -0,0 +1,39 @@
+piramide = {
+   'teoremi che se non sai vieni bocciato!' : [
+      'teorema inverso (e diretto) di bolzano (dimostrazione: SI ',
+      'teorema ponte (dimostrazione: SI ?)',
+      ],
+   'teoremi sulle derivate' : [
+      'teorema di weirstrass dimostrazione (SI)',
+      'teorema di fermat dimostrazione (SI)',
+      'teorema di lagrange dimostrazione (SI)',
+      'teorema di rolle dimostrazione (SI)',
+      'teorema di cauchy dimostrazione (SI)',
+      'teorema di de l\'hopital',
+      ]
+   'teoremi sui limiti': [
+      'teorema del confronto e corollari vari dimostrazione (SI)',
+      'teorema di unicità del limite',
+      'teorema dei valori intermedi',
+      'teorema degli zeri',
+      'teorema della permanenza del segno'
+      ]
+   
+   'teoremi sul calcolo integrale' : [
+      'teorema della media integrale',
+      'teorema fondamentale del calcolo integrale'
+      ]
+   'definizioni relative ai limiti (so na cifra, vedi quaderno E slide)': [
+      'definizione di limite di successione convergente, divergente',
+      'definzione di limite di funzione convergente, divergente',
+      'definizione di punto di accumulazione',
+      'definizione di compatto',
+      'definizione di intorno',
+      'definzione di continuità',
+      'condizioni sufficienti per la derivabilità',
+      'condizioni sufficienti per l\'integrabilità'
+      ]
+   'teoremi sensa senso': [
+      'teorema di cantor (quello vecchio incomprensibile)',
+      ]
+   }
